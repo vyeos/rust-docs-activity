4cce45170acf55c4fe57230055909b5705699951
vyeos <rudranpatel0980@gmail.com>
2026-05-14 20:02:16 +0530

    1.0 Hello World

---
+++ 1.0 Hello World/src/main.rs
@@ -0,0 +1,8 @@
+// Click ‘Run’ above to see the expected output. Next, add a new line with a second println! macro so that the output shows:
+// Hello World!
+// I'm a Rustacean!
+
+fn main() {
+    println!("Hello world!");
+    println!("I'm a Rustacean!")
+}
